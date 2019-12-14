ca2489767dddcb6518b536f0cdce87b557e88785
Garret Meier <garret@mixmax.com>
2019-12-13 17:26:53 -0800

    chore: add jenkins

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 const jestCoverageConfig = require('@mixmaxhq/jest-coverage-config');
+const jestJunitConfig = process.env.CI && require('@mixmaxhq/jest-junit-config');
+
 module.exports = {
   testEnvironment: 'node',
   clearMocks: true,
   ...jestCoverageConfig,
   collectCoverageFrom: ['<rootDir>/src/**/*.js'],
+  ...jestJunitConfig,
 };
